5f5ef3f28b8dcae411364c6b0c56d7f0cb603cf1
Steve van Bennekom <steve@MacBook-Air-van-Steve.local>
2025-01-06 06:03:22 +0100

    Create event classes by macro's

--- source/event.h
+++ source/event.h
@@ -1,6 +1,7 @@
 #ifndef SHIPZ_EVENT_H
 #define SHIPZ_EVENT_H
 
+#include <iostream>
 #include <SDL3/SDL.h>
 #include <string>
 
@@ -19,62 +20,178 @@ enum EVENT_TYPE {
 // A game event
 class Event : public Message {
     public:
-        virtual ~Event();
+        virtual ~Event() {
+            // Destructor implementation (even if empty)
+        }
         // Serialize event into a buffer - should return false if buffer too small
         bool Serialize(Buffer *);
         // Deserialize buffer into a event
         static Event* Deserialize(Buffer *);
 };
 
-class EventPlayerJoins : public Event {
-    public:
-        Uint8 player_number;
-        std::string player_name;
-        bool Serialize(Buffer *);
+//      Base class  Class name   Header
+#define MESSAGES_EVENTS \
+    MESSAGE_CLASS(Event, PlayerJoins,   PLAYER_JOINS,   EVENT) \
+    MESSAGE_CLASS(Event, PlayerLeaves,  PLAYER_LEAVES,  EVENT) \
+    MESSAGE_CLASS(Event, TeamWins,      TEAM_WINS,      EVENT) \
+    MESSAGE_CLASS(Event, LevelChanges,  LEVEL_CHANGE,   EVENT) \
+    MESSAGE_CLASS(Event, ServerQuits,   SERVER_QUIT,    EVENT) \
 
-        // Constructor
-        EventPlayerJoins(Uint8, const char *);
-        static Event * Deserialize(Buffer *buffer);
+#define FIELDS_PlayerJoins(field_handler) \
+    field_handler(FIELD_UINT8, player_number)\
+    field_handler(FIELD_STRING, player_name)
+
+#define FIELDS_PlayerLeaves(field_handler) \
+    field_handler(FIELD_UINT8, player_number)\
+    field_handler(FIELD_STRING, leave_reason)
+
+#define FIELDS_TeamWins(field_handler) \
+    field_handler(FIELD_UINT8, team)\
+
+#define FIELDS_LevelChanges(field_handler) \
+    field_handler(FIELD_STRING, filename)\
+    field_handler(FIELD_STRING, message)
+
+#define FIELDS_ServerQuits(field_handler) \
+    field_handler(FIELD_STRING, message)
+
+#define FIELD_UINT8_TYPE Uint8
+#define FIELD_UINT16_TYPE Uint16
+#define FIELD_UINT32_TYPE Uint32
+#define FIELD_STRING_TYPE std::string
+#define FIELD_UINT8_SIZE(value) 1
+#define FIELD_UINT16_SIZE(value) 2
+#define FIELD_UINT32_SIZE(value) 4
+#define FIELD_STRING_SIZE(value) value.length() + 1
+#define FIELD_UINT8_MINSIZE 1
+#define FIELD_UINT16_MINSIZE 2
+#define FIELD_UINT32_MINSIZE 4
+#define FIELD_STRING_MINSIZE 1
+#define FIELD_UINT8_SERIALIZE(value) Write8(value)
+#define FIELD_UINT16_SERIALIZE(value) Write16(value)
+#define FIELD_UINT32_SERIALIZE(value) Write32(value)
+#define FIELD_STRING_SERIALIZE(value) WriteString(value.c_str())
+#define FIELD_UINT8_DESERIALIZE Read8()
+#define FIELD_UINT16_DESERIALIZE Read16()
+#define FIELD_UINT32_DESERIALIZE Read32()
+#define FIELD_STRING_DESERIALIZE ReadString()
+
+// Field handler that expands field definition into variable declarations;
+#define MESSAGE_FIELD_DECLARATION(type, name) type##_TYPE name;
+
+// Field handler that expands field definition into parameter type list;
+#define MESSAGE_FIELD_ARGUMENT_LIST(type, name) type##_TYPE,
+
+// Field handler that expands field definition into parameter name list;
+#define MESSAGE_FIELD_ARGUMENT_NAME_LIST(type, name) name,
+
+// Field handler that expands field definition into constructor initializer statements;
+#define MESSAGE_FIELD_INIT(type, name) this->name = name;
+
+// Field handler that expands field definition into buffer writes;
+#define SERIALIZE_FIELDS_TO_BUFFER(type, name) \
+    buffer->type##_SERIALIZE(name);
+
+// Field handler that expands field definition into a required write size;
+#define SUM_REQUIRED_FIELD_SIZE(type, name) \
+    type##_SIZE(name) +
+
+// Field handler that expands field definition into buffer reads;
+#define DESERIALIZE_FIELDS_FROM_BUFFER(type, name) \
+    if( buffer->AvailableRead() < type##_MINSIZE ) { \
+        return NULL; \
+    } \
+    type##_TYPE name = buffer->type##_DESERIALIZE;
+
+
+#define MESSAGE_CLASS(BASE_CLASS, CLASS_NAME, HEADER, BASE_HEADER) \
+class BASE_CLASS##CLASS_NAME : public BASE_CLASS{ \
+    public: \
+        /* Add field declarations */ \
+        FIELDS_##CLASS_NAME(MESSAGE_FIELD_DECLARATION) \
+        \
+        /* Constructor */ \
+        BASE_CLASS##CLASS_NAME(FIELDS_##CLASS_NAME(MESSAGE_FIELD_ARGUMENT_LIST) ...) {\
+            this->SetMessageType(MessageType::BASE_HEADER); \
+            this->SetMessageSubType(HEADER); \
+            FIELDS_##CLASS_NAME(MESSAGE_FIELD_INIT) \
+        }\
+        \
+        /* Serialize Message */ \
+        bool Serialize(Buffer *buffer); \
+        \
+        /* Deserialize Message*/ \
+        static BASE_CLASS* Deserialize(Buffer *buffer); \
 };
 
-class EventPlayerLeaves : public Event {
-    public:
-        enum LEAVE_REASON {
-            QUIT,
-            TIMEOUT,
-            KICKED
-        };
-        Uint8 player_number;
-        Uint8 reason;
-        bool Serialize(Buffer *buffer);
+MESSAGES_EVENTS
+#undef MESSAGE_CLASS
 
-        EventPlayerLeaves(Uint8 number, Uint8 leave_reason);
-        static Event * Deserialize(Buffer *buffer);
-};
+// Implement Serialization methods
+#define MESSAGE_CLASS(BASE_CLASS, CLASS_NAME, HEADER, BASE_HEADER) \
+    bool BASE_CLASS##CLASS_NAME::Serialize(Buffer *buffer) { \
+        if( buffer->AvailableWrite() < ( FIELDS_##CLASS_NAME(SUM_REQUIRED_FIELD_SIZE) 0) ) { \
+            return false; \
+        } \
+        FIELDS_##CLASS_NAME(SERIALIZE_FIELDS_TO_BUFFER) \
+        return true;\
+    }
 
-class EventTeamWins : public Event {
-    public:
-        Uint8 team;
-        bool Serialize(Buffer *);
-        EventTeamWins(Uint8);
-        static Event * Deserialize(Buffer *buffer);
-};
+MESSAGES_EVENTS
+#undef MESSAGE_CLASS
 
-class EventLevelChange : public Event {
-    public:
-        std::string level;
-        std::string message;
-        bool Serialize(Buffer *);
-        EventLevelChange(const char *, const char *);
-        static Event * Deserialize(Buffer *buffer);
-};
+// Implement Deserialization methods
+#define MESSAGE_CLASS(BASE_CLASS, CLASS_NAME, HEADER, BASE_HEADER) \
+    BASE_CLASS* BASE_CLASS##CLASS_NAME::Deserialize(Buffer *buffer) { \
+        if(buffer->AvailableRead() < 1) { \
+            return NULL; \
+        } \
+        /* Consume event type header */ \
+        buffer->Read8(); \
+        /* Deserialize fields */ \
+        FIELDS_##CLASS_NAME(DESERIALIZE_FIELDS_FROM_BUFFER) \
+        return new BASE_CLASS##CLASS_NAME(FIELDS_##CLASS_NAME(MESSAGE_FIELD_ARGUMENT_NAME_LIST) NULL); \
+    }
 
-class EventServerQuit : public Event {
-    public:
-        std::string message;
-        EventServerQuit(const char *);
-        bool Serialize(Buffer *);
-        static Event * Deserialize(Buffer *buffer);
-};
+MESSAGES_EVENTS
+#undef MESSAGE_CLASS
+
+#define MESSAGE_CLASS(BASE_CLASS, CLASS_NAME, HEADER, BASE_HEADER) \
+    case HEADER:\
+        return static_cast<BASE_CLASS##CLASS_NAME*>(this)->Serialize(buffer); \
+        break; \
+
+
+// Implement Deserialization from base class
+bool Event::Serialize(Buffer * buffer) {
+    switch(this->GetMessageSubType()) {
+        MESSAGES_EVENTS
+        default:
+        std::cout << "debug: Unknown event type" << std::endl;
+        return false;
+    }
+    return false;
+}
+#undef MESSAGE_CLASS
+#define MESSAGE_CLASS(BASE_CLASS, CLASS_NAME, HEADER, BASE_HEADER) \
+    case HEADER: \
+        event = BASE_CLASS##CLASS_NAME::Deserialize(buffer); \
+        break; \
+
+// Factory function to Deserialize an event
+// Returns an event type based on the type 
+// of event deserialized
+Event* Event::Deserialize(Buffer *buffer) {
+    Event *event = NULL;
+
+    switch(GetSubMessageTypeFromHeader(buffer->Peek8())) {
+        MESSAGES_EVENTS
+        default:
+            std::cout << "debug: Unknown event type" << std::endl;
+            return NULL;
+    }
+    return event;
+}
+
 
 #endif
--- tests/event_test.cpp
+++ tests/event_test.cpp
@@ -20,3 +20,21 @@ TEST(Simple, TestEventPlayerJoinsRoundTrip) {
     ASSERT_EQ(deserialized->player_number, 42);
     ASSERT_STREQ(deserialized->player_name.c_str(), "player_nickname");
 }
+
+TEST(Simple, TestEventPlayerLeavesRoundTrip) {
+    Buffer buffer;
+    buffer.Clear();
+    auto event = new EventPlayerJoins(42, "player_nickname"); 
+    EventPlayerJoins * deserialized;
+    
+    event->Serialize(&buffer);
+
+    ASSERT_TRUE(buffer.length > 5);
+    buffer.Seek(0);
+
+    deserialized = static_cast<EventPlayerJoins*>(Event::Deserialize(&buffer));
+
+    ASSERT_EQ(deserialized->GetMessageSubType(), PLAYER_JOINS);
+    ASSERT_EQ(deserialized->player_number, 42);
+    ASSERT_STREQ(deserialized->player_name.c_str(), "player_nickname");
+}
